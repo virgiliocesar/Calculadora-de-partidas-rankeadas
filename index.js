669e414913625c4f78f037214cfda84299cc1ec9
virgílio conceição césar <virgilio_cesar.dev@outlook.com>
2024-10-16 21:14:00 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-let victory = 10;
-let defeat = 5;
-let stopp = ""
-
+let victory = 40;
+let defeat = 10;
+let stopp = "";
 
 function ranking(victory, defeat) {
-    while (stopp === "") {
-      if (victory < 10) {
-        return console.log("Ferro");
-      } else if (victory >= 11 && victory <= 20) {
-        return console.log("Bronze");
-      } else if (victory >= 21 && victory <= 50) {
-        return console.log("Prata");
-      } else if (victory >= 51 && victory <= 80) {
-        return console.log("Ouro");
-      } else if (victory >= 81 && victory <= 90) {
-        return console.log("Diamante");
-      } else if (victory >= 91 && victory <= 100) {
-        return console.log("Lendário");
-      } else if (victory >= 101) {
-        return console.log("Imortal");
-      }
+  const winningBalance = victory - defeat;
+
+  //   while (stopp === "") {
+  let nivel;
+  if (victory < 10) {
+    nivel = "Ferro";
+  } else if (victory >= 11 && victory <= 20) {
+    nivel = "Bronze";
+  } else if (victory >= 21 && victory <= 50) {
+    nivel = "Prata";
+  } else if (victory >= 51 && victory <= 80) {
+    nivel = "Ouro";
+  } else if (victory >= 81 && victory <= 90) {
+    nivel = "Diamante";
+  } else if (victory >= 91 && victory <= 100) {
+    nivel = "Lendário";
+  } else if (victory >= 101) {
+    nivel = "Imortal";
   }
+  return console.log(`Imortal ${winningBalance} está no nivel de ${nivel}`);
 }
+// }
 
 ranking(victory, defeat);
-
-
-
-
-
